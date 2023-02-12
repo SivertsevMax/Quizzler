bfc39f3ca70762f72028dbd28db5db2802458841
SivertsevMax <sivertsev1995@icloud.com>
2023-02-12 08:12:25 +0300

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Максим Сиверцев on 12.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
